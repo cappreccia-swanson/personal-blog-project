c30e598ffe41cd85bb3f5e878b79a3012f7ef145
Cappreccia Swanson <capprecciaswanson@gmail.com>
2023-01-04 10:16:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Back to Top Button
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 // Read More and Less Button
 function myFunction() {
   var dots = document.getElementById("dots");
